5fa4e2d4ca66d0c4bc880ae880b1e54f45af70f4
Mamun123123123 <almalm90459@gmail.com>
2026-03-11 12:55:22 +0600

    Learn about useNavigate hook

--- practice_react/src/Pages/About.jsx
+++ practice_react/src/Pages/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
+    let navigate = useNavigate()
   return (
     <div>
        <h1>About Page..............</h1>
+       <button onClick={()=>{
+        navigate('/')
+       }}>Back to Home</button>
+       <button onClick={()=>{
+           navigate(-1)
+       }}>Back</button>
     </div>
   )
 }
