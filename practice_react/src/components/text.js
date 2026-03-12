8181d88f9494771df99ecc2f2353851e7ee3cb65
Mamun123123123 <almalm90459@gmail.com>
2026-03-12 19:42:51 +0600

    find duplicate value using loop in array

--- practice_react/src/components/text.js
+++ practice_react/src/components/text.js
@@ -1,13 +1,23 @@
-let arr = [15,25,35,30,60]
+// let arr = [15,25,35,30,60]
 
-for(let i=0;i<arr.length;i++)
- {
-    for(let j=i+1;j<arr.length;j++){
-        for(let k=j+1;k<arr.length;k++){
-            if(arr[i]+arr[j]+arr[k] === 125 ){
-                console.log(arr[i],arr[j],arr[k]);
+// for(let i=0;i<arr.length;i++)
+//  {
+//     for(let j=i+1;j<arr.length;j++){
+//         for(let k=j+1;k<arr.length;k++){
+//             if(arr[i]+arr[j]+arr[k] === 125 ){
+//                 console.log(arr[i],arr[j],arr[k]);
                 
-            }
-        }
-    }
- }
+//             }
+//         }
+//     }
+//  }
+
+let arr = [1,2,3,4,5,6,2,5]
+let duplicate = []
+for(let i=0;i<arr.length;i++){
+     if(duplicate.includes(arr[i])){
+        console.log(arr[i]);
+     }else{
+        duplicate.push(arr[i])
+     }
+}
