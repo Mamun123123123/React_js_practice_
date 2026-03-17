0d6c7ad20aeabc06f684003d51eecb980a16f3c0
Mamun123123123 <almalm90459@gmail.com>
2026-03-17 06:25:38 +0600

    add pagination logic

--- practice_react/src/Project4/Country.jsx
+++ practice_react/src/Project4/Country.jsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react'
 
 const Country = () => {
      const [data,setData] = useState([])
+     const [currentPage, setCurrentPage] = useState(1);
+
+     const itemsPerPage = 12;
      const allCountry = async()=>{
       let response = await axios.get("https://openapi.programming-hero.com/api/all")
       console.log(response.data.countries);
@@ -11,10 +14,13 @@ const Country = () => {
      useEffect(()=>{
         allCountry()
      },[])
+      const lastIndex = currentPage * itemsPerPage;
+      const firstIndex = lastIndex - itemsPerPage;
+      const currentCountries = data.slice(firstIndex, lastIndex);
   return (
     <div>
         <div className='bg-amber-50 items-center p-2 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 text-black'>
-            {data.map((item,index)=>(
+            {currentCountries.map((item,index)=>(
                 <div key={index} className='bg-blue-300 p-2 m-4 flex flex-col items-center rounded-2xl'>
                 <img className='w-60 rounded-full object-cover p-4' src={item.flags.flags.png} alt="img" />
                 <h1 className='text-2xl tracking-widest'>{item.name.common}</h1>
@@ -27,6 +33,22 @@ const Country = () => {
                 
             ))}
         </div>
+        <div className="flex justify-center bg-slate-50 gap-4 p-6">
+        <button
+          className="bg-blue-500 text-white p-2 rounded"
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Prev
+        </button>
+
+        <button
+          className="bg-blue-500 text-white p-2 rounded"
+          onClick={() => setCurrentPage(currentPage + 1)}
+        >
+          Next
+        </button>
+      </div>
     </div>
   )
 }
