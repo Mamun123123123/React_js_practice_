9c9cf6c7966aa6c70c5348467516d3b5731be7e1
Mamun123123123 <almalm90459@gmail.com>
2026-03-27 11:55:45 +0600

    create active button

---
+++ practice_react/src/Dream_BPL_11/Available.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Available = ({item}) => {
+  return (
+    <>
+    <div 
+            className="bg-white rounded-2xl shadow-lg p-4 hover:shadow-2xl transition duration-300"
+          >
+            <img 
+              src={item.img} 
+              alt={item.playername}
+              className="w-full h-48 object-cover rounded-xl"
+            />
+
+            <h1 className="text-xl font-bold mt-3 text-gray-800">
+              {item.playername}
+            </h1>
+            <div className="flex justify-between items-center mt-2">
+              <span className="text-gray-600 text-sm">
+                {item.playercountry}
+              </span>
+
+              <button className="bg-blue-500 text-white px-2 py-1 text-xs rounded-full">
+                {item.playertype}
+              </button>
+            </div>
+            <div className="mt-3 text-sm text-gray-700 space-y-1">
+              <p>Rating: {item.rating}</p>
+              <p>Batting: {item.battingstyle}</p>
+              <p>Bowling: {item.bowlingstyle}</p>
+            </div>
+            <div className="flex justify-between items-center mt-4">
+              <p className="font-semibold text-green-600">
+                ${item.price}
+              </p>
+
+              <button className="bg-green-500 hover:bg-green-600 text-white px-3 py-1 rounded-lg text-sm">
+                Choose
+              </button>
+            </div>
+
+          </div>
+      
+    </>
+  )
+}
+
+export default Available
